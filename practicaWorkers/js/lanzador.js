f86970f1d027ebe18ec9add721eb23ff077e3dd1
TubalLara <tubal.lara@tajamar365.com>
2014-10-13 11:51:01 +0200

    practica

---
+++ practicaWorkers/js/lanzador.js
@@ -0,0 +1,33 @@
+﻿var trabajador;
+
+var urlprovi = [];
+
+foten.src = urlprovi;
+
+
+function iniciar() {
+    
+    
+    if (typeof (Worker) !== "undefined") {
+        trabajador = new Worker("js/worker.js");
+
+    }
+
+
+    trabajador.onmessage = function (evt) {
+        urlprovi = evt.data;
+        
+        
+    };
+
+    trabajador.onerror = function (err) {
+        alert(err.data);
+
+    };
+
+}
+
+(function () {
+    iniciar();
+
+})();
---
+++ practicaWorkers/js/worker.js
@@ -0,0 +1,17 @@
+﻿var url0 = "http://static.freepik.com/foto-gratis/casa_17-1008174100.jpg";
+var url1 = "http://static.freepik.com/foto-gratis/icono-de-la-casa_17-1126210653.jpg";
+var i;
+var url;
+
+function cambiarFoto() {
+    
+    for (i = 1; i <= 1; i++) {
+        url.id = "url" + [i];
+        
+    };
+
+    postMessage(url);
+    setTimeout("cambiarFoto()", 3000);
+    
+
+};
